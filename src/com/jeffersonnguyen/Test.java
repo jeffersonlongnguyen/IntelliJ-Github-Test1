49bd0e5c6ce309a3da403535802594bccf308f6f
Jefferson Nguyen <jeffersonlongnguyen@gmail.com>
2020-10-20 00:51:54 -0700

    Test

---
+++ src/com/jeffersonnguyen/Test.java
@@ -0,0 +1,13 @@
+package com.jeffersonnguyen;
+
+/**
+ * This program was created on the computer as a local project and will be uploaded to Github
+ */
+
+public class Test
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello, World");
+    }
+}
